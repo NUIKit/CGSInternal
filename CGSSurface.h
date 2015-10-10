bf907624246d929d9a4ea26e462ac7d9ad65a31b
Robert Widmann <devteam.codafi@gmail.com>
2015-10-10 00:41:10 -0400

    Add surface opacity and resolution setters

--- CGSSurface.h
+++ CGSSurface.h
@@ -20,15 +20,20 @@ CG_EXTERN CGError CGSRemoveSurface(CGSConnectionID cid, CGWindowID wid, CGSSurfa
 
 /*! Sets the bounds of a surface. */
 CG_EXTERN CGError CGSSetSurfaceBounds(CGSConnectionID cid, CGWindowID wid, CGSSurfaceID sid, CGRect bounds);
-
 /*! Gets the smallest rectangle a surface's frame fits in. */
 CG_EXTERN CGError CGSGetSurfaceBounds(CGSConnectionID cid, CGWindowID wid, CGSSurfaceID sid, CGFloat *bounds);
 
 /*! Gets the count of all drawable surfaces on a window. */
 CG_EXTERN CGError CGSGetSurfaceCount(CGSConnectionID cid, CGWindowID wid, int *outCount);
-
 /*! Gets a list of surfaces owned by a window. */
 CG_EXTERN CGError CGSGetSurfaceList(CGSConnectionID cid, CGWindowID wid, int countIds, CGSSurfaceID *ids, int *outCount);
+
+CG_EXTERN CGError CGSSetSurfaceResolution(CGSConnectionID cid, CGWindowID wid, CGSSurfaceID sid, CGFloat scale);
+
+CG_EXTERN CGError CGSSetSurfaceOpacity(CGSConnectionID cid, CGWindowID wid, CGSSurfaceID sid, bool isOpaque);
+
+/// Currently does nothing.
+CG_EXTERN CGError CGSSetSurfaceBackgroundBlur(CGSConnectionID cid, CGWindowID wid, CGSSurfaceID sid, CGFloat blur) AVAILABLE_MAC_OS_X_VERSION_10_11_AND_LATER;
 
 /*! Sets the order of a surface relative to another surface. */
 CG_EXTERN CGError CGSOrderSurface(CGSConnectionID cid, CGWindowID wid, CGSSurfaceID surface, CGSSurfaceID otherSurface, int place);
